95e377556ab474a7dbdb1c2515303a0c43102dae
AliJabrailzade <jabrailzadeali@gmail.com>
2021-05-14 22:34:24 +0400

    added Delete all tasks to myTasks

--- myTasks/views.py
+++ myTasks/views.py
@@ -13,11 +13,16 @@ def myTasks(request):
     if "tasks" not in request.session:
         request.session["tasks"] = []
     if request.POST:
-        print(f"this is the post method: {request.POST['f']}")
-        print(request.session['tasks'])
-        request.session['tasks'] += [request.POST['f']] # if we use don't convert it to list(request.POST['f']) it will add all indivial characters to the list not strings
-        print(request.session['tasks'])
-        return HttpResponseRedirect(reverse('myTasks'))
+        if 'add_task' in request.POST:
+            print(f"this is the post method: {request.POST['f']}")
+            print(request.session['tasks'])
+            request.session['tasks'] += [request.POST['f']] # if we use don't convert it to list(request.POST['f']) it will add all indivial characters to the list not strings
+            print(request.session['tasks'])
+            return HttpResponseRedirect(reverse('myTasks'))
+        elif 'delete_task' in request.POST:
+            print('hello multiple forms --------------------')
+            request.session['tasks'] = []
+            return HttpResponseRedirect(reverse('myTasks'))
     else:
         return render(request, 'myTasks/index.html' ,{
             'form': form,
