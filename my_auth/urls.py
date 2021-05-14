978a50892162a7da8bebd5365e89bdaf81c80110
AliJabrailzade <jabrailzadeali@gmail.com>
2021-05-14 18:42:24 +0400

    add my_auth

---
+++ my_auth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('create_account/', views.create_account, name='create_account'),
+]
